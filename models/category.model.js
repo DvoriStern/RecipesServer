e8b087ba6a0910c49a8a8c9d84a0c392026010e1
DvoriStern <dvori8847@gmail.com>
2024-05-09 19:51:26 +0300

    category model

---
+++ models/category.model.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+
+const recipeSchema=new mongoose.Schema({
+    _id:{type:mongoose.Types.ObjectId,required:true},
+    name:{type:String,required:true}
+})
+const categorySchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    recipes:{type:[recipeSchema],required:true}
+});
